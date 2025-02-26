0061970ad1e8ee8772a3be2baffe45ca5f07876d
kyontan <kyontan@monora.me>
2025-02-26 22:06:20 +0900

    chore: append command name to the error message

--- main.go
+++ main.go
@@ -58,13 +58,13 @@ func main() {
 	secret_id := os.Getenv("WITH_SECRET_ID")
 
 	if secret_id == "" {
-		fmt.Fprintln(os.Stderr, "Error: WITH_SECRET_ID is not set")
+		fmt.Fprintln(os.Stderr, "[with-secret] Error: WITH_SECRET_ID is not set")
 		PrintUsageAndExit()
 	}
 
 	// check if command is provided
 	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "Error: command is not provided")
+		fmt.Fprintln(os.Stderr, "[with-secret] Error: command is not provided")
 		PrintUsageAndExit()
 	}
 
@@ -83,7 +83,7 @@ func main() {
 	secret_map := make(map[string]string)
 	err := json.Unmarshal(secret_json, &secret_map)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: secret is not JSON or not key-value pairs: %+v\n", err)
+		fmt.Fprintf(os.Stderr, "[with-secret] Error: secret is not JSON or not key-value pairs: %+v\n", err)
 		os.Exit(1)
 	}
 
@@ -105,18 +105,18 @@ func main() {
 
 	stdout, err := command.StdoutPipe()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: unable to get stdout pipe: %+v\n", err)
+		fmt.Fprintf(os.Stderr, "[with-secret] Error: unable to get stdout pipe: %+v\n", err)
 		os.Exit(1)
 	}
 
 	stderr, err := command.StderrPipe()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: unable to get stderr pipe: %+v\n", err)
+		fmt.Fprintf(os.Stderr, "[with-secret] Error: unable to get stderr pipe: %+v\n", err)
 		os.Exit(1)
 	}
 
 	if err := command.Start(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: unable to start command: %+v\n", err)
+		fmt.Fprintf(os.Stderr, "[with-secret] Error: unable to start command: %+v\n", err)
 		os.Exit(1)
 	}
 
@@ -161,7 +161,7 @@ func main() {
 	wg.Wait()
 
 	if err := command.Wait(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: command execution failed: %+v\n", err)
+		fmt.Fprintf(os.Stderr, "[with-secret] Error: command execution failed: %+v\n", err)
 		os.Exit(1)
 	}
 }
